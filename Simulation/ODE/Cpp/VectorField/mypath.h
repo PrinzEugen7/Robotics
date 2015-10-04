22e08b926f248dd8e0993b261e3ac23ad14b4790
PrinzEugen7 <nsi05162@pooae.com>
2015-10-04 12:35:18 +0900

    Update mypath.h

--- Simulation/ODE/Cpp/VectorField/mypath.h
+++ Simulation/ODE/Cpp/VectorField/mypath.h
@@ -168,17 +168,15 @@ void loadMap(Mat im){
 using namespace cv;
 
 void obstDetection(Mat im){
-    hsv, mask;					        // 画像オブジェクトの宣言
-    cvtColor(im, hsv, CV_BGR2HSV);      // 画像をRGBからHSVに変換
-    inRange(hsv, Scalar(150, 70, 70), Scalar(360, 255, 255), mask);	// 色検出でマスク画像の作成
-    imshow("Mask", mask);               // マスク画像の作成
-    waitKey(0);
+	hsv, mask;					        // 画像オブジェクトの宣言
+	cvtColor(im, hsv, CV_BGR2HSV);      // 画像をRGBからHSVに変換
+	inRange(hsv, Scalar(150, 70, 70), Scalar(360, 255, 255), mask);	// 色検出でマスク画像の作成
 	int h = mask.rows;
 	int w = mask.cols;
 	for (int y = 0; y < h; y++){
 		for (int x = 0; x < w; x++){
 			if (mask.data[y * h + x] == 255){
-
+				wall.saveTxt("obst.csv",x,y); 
 			}
 		}
 	}
