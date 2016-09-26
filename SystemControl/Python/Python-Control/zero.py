3ccbdc60979682f2b22ba9cc67d4eea10a30cc2e
PrinzEugen7 <nsi05162@pooae.com>
2016-09-26 22:28:09 +0900

    Create zero.py

---
+++ SystemControl/Python/Python-Control/zero.py
@@ -0,0 +1,14 @@
+from control.matlab import *
+
+def main():
+  A = "0 1; -1 -1"
+  B = "0; 1"
+  C= "2 0"
+  D = "0"
+  # 状態空間モデルの作成
+  sys = ss(A, B, C, D)
+  zeros = zero(sys)
+  print(zeros)
+  
+if __name__ == "__main__":
+    main()
