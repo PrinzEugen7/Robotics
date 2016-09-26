e32a86ab6f38658dcc80d9b825933312af37bf94
PrinzEugen7 <nsi05162@pooae.com>
2016-09-26 23:37:11 +0900

    システムの可制御性を判定します。
    システムの可制御性を判定します。

--- SystemControl/Python/Python-Control/ctrb.py
+++ SystemControl/Python/Python-Control/ctrb.py
@@ -2,15 +2,17 @@ from control.matlab import *
 import numpy as np
 
 def main():
+  # システム行列の定義
   A = "0 0; 0 -1"
   B = "1; 1"
   C= "2 0"
   D = "0"
   # 状態空間モデルの作成
   sys = ss(A, B, C, D)
-  Uc =ctrb(A,B) # 
+  Uc =ctrb(A, B) # 可制御性行列の計算
   Nu = np.linalg.matrix_rank(Uc)  # Ucのランクを計算
-  N = 2 # 正方行列Aのサイズ(2*2)
+  (N, N) = np.matrix(A).shape     # 正方行列Aのサイズ(N*N)
+  # 可制御性の判別
   if Nu == N: 
     print("システムは可制御である") 
   else: 
