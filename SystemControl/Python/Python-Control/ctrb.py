6cccb12e0aeb2ea4fab99d8457e4c5747c4e7978
PrinzEugen7 <nsi05162@pooae.com>
2016-09-27 20:12:18 +0900

    可制御性のチェック
    可制御性のチェック

--- SystemControl/Python/Python-Control/ctrb.py
+++ SystemControl/Python/Python-Control/ctrb.py
@@ -3,13 +3,13 @@ import numpy as np
 
 def main():
   # システム行列の定義
-  A = "0 0; 0 -1"
-  B = "1; 1"
-  C= "2 0"
+  A = "0 1; -1 -1"
+  B = "0; 1"
+  C= "1 0"
   D = "0"
   # 状態空間モデルの作成
   sys = ss(A, B, C, D)
-  Uc =ctrb(A, B) # 可制御性行列の計算
+  Uc = ctrb(A, B) # 可制御性行列の計算
   Nu = np.linalg.matrix_rank(Uc)  # Ucのランクを計算
   (N, N) = np.matrix(A).shape     # 正方行列Aのサイズ(N*N)
   # 可制御性の判別
@@ -20,4 +20,4 @@ def main():
 
   
 if __name__ == "__main__":
-    main()
+  main()
