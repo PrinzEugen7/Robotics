bb9393a6ed85be06433bef5659b8a9d3e54c517b
PrinzEugen7 <nsi05162@pooae.com>
2016-09-28 22:04:55 +0900

    根軌跡のプロット
    根軌跡のプロット

---
+++ SystemControl/Python/rlocus.py
@@ -0,0 +1,13 @@
+from control.matlab import *
+from matplotlib import pyplot as plt
+    
+def main():
+  # 伝達関数のパラメータ
+  num = [2, 5, 1]     # 分子の係数
+  den = [1, 2, 3]     # 分母の係数
+  sys = tf(num, den)  # 伝達関数モデルの作成
+  rlocus(sys)         # 根軌跡のプロット
+  plt.show()
+
+if __name__ == "__main__":
+  main()
